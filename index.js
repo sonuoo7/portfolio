4522b7c87935ba8c36955c3a5fe73dd7d4e99296
Sonu Pant007 <60938647+sonuoo7@users.noreply.github.com>
2022-12-03 20:00:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log(a)
+console.log(b)
+
+
+var a=10;
+
+
+console.log(a)
+console.log(b)
+
+
+var b=20;
+
+
+console.log(a)
+console.log(b)
+
+
+console.log(a)
+console.log(b)
+console.log(c)
